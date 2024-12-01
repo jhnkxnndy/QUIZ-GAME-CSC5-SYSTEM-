70c084cda2e6bfdfe7a2e3f8c8501e45930ce101
Itsmekyky <163654270+Itsmekyky@users.noreply.github.com>
2024-12-01 17:35:10 +0800

    Update LoginSuccess.vb

--- LoginSuccess.vb
+++ LoginSuccess.vb
@@ -1,15 +1,15 @@
 ﻿Public Class LoginSuccess
-    Public Property Username As String ' Property to store the username
+    Public Property Username As String 
 
     Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
-        ' Create a new instance of MainInterface
+        
         Dim mainInterface As New MainInterface()
 
-        ' Set the UserLabel text to the username
+       
         mainInterface.UserLabel.Text = Username
 
-        mainInterface.Show() ' Show the MainInterface
-        Me.Hide() ' Hide the LoginSuccess form
+        mainInterface.Show() 
+        Me.Hide()
         SignInForm.Hide()
     End Sub
 End Class
