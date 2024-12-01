58656a47d0d721d40946bcd843a7b46670e9c03f
Itsmekyky <163654270+Itsmekyky@users.noreply.github.com>
2024-12-01 18:03:01 +0800

    Update MCQuestion9.vb

--- MCQuestion9.vb
+++ MCQuestion9.vb
@@ -9,16 +9,16 @@ Public Class MCQuestion9
 
     Private flickerCount As Integer = 0
     Private flickerTimer As New Timer() With {.Interval = 100}
-    Private transitionTimer As New Timer() With {.Interval = 2000} ' 2-second intervals
-    Private currentPhase As Integer = 0 ' Track which part of the transition we are in
-    Private buttonsClickable As Boolean = True ' Track if buttons are clickable
+    Private transitionTimer As New Timer() With {.Interval = 2000} 
+    Private currentPhase As Integer = 0 
+    Private buttonsClickable As Boolean = True 
     Private correctSound As New SoundPlayer("C:\Users\johnk\Downloads\Untitled video - Made with Clipchamp (1).wav")
     Private wrongSound As New SoundPlayer("C:\Users\johnk\Downloads\Untitled video - Made with Clipchamp (2).wav")
     Private Sub UpdatePanelColor(panelName As String, newColor As Color)
         If PanelColors.ContainsKey(panelName) Then
-            PanelColors(panelName) = newColor ' Update the global dictionary
+            PanelColors(panelName) = newColor 
 
-            ' Apply the color to the actual panel
+            
             Select Case panelName
                 Case "Q9Panel"
                     QuestionScores.Q9Panel.FillColor = newColor
@@ -27,28 +27,28 @@ Public Class MCQuestion9
     End Sub
 
     Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
-        ' Prevent multiple clicks by disabling the button
+        
         If Not buttonsClickable Then Return
         buttonsClickable = False
 
-        ' Increment the correct answers count and add to AnsClass
+       
         CorrectAnswers.CorrectAnswersCount += 1
         AnsClass.AddCorrectAnswer(True)
 
-        ' Update the user's score
+       
         UpdateScore(UserSession.Username, 20)
 
-        ' Set up flickering effect and change button color to LightGreen
+       
         flickerCount = 0
         Guna2Button1.FillColor = Color.LightGreen
         UpdatePanelColor("Q9Panel", Color.LightGreen)
-        QuestionScores.Q9Panel.FillColor = Color.LightGreen ' Change the panel color for the correct answer
+        QuestionScores.Q9Panel.FillColor = Color.LightGreen 
         flickerTimer.Start()
         correctSound.Play()
-        ' Show CorrectForm after flickering
+        
         Dim correctForm As New CorrectForm()
         correctForm.Show()
-        transitionTimer.Start() ' Start transition timer to handle closing the CorrectForm
+        transitionTimer.Start() 
     End Sub
 
 
@@ -150,7 +150,7 @@ Public Class MCQuestion9
         Else
             flickerTimer.Stop()
             Guna2Button1.FillColor = Color.LightGreen
-            QuestionScores.Q9Panel.FillColor = Color.LightGreen ' Set the panel color as well
+            QuestionScores.Q9Panel.FillColor = Color.LightGreen 
             currentPhase = 1
         End If
     End Sub
@@ -158,7 +158,7 @@ Public Class MCQuestion9
     Private Sub TransitionSequence(sender As Object, e As EventArgs)
         Select Case currentPhase
             Case 1
-                ' Close WrongForm or CorrectForm
+                
                 Dim correctForm As CorrectForm = Application.OpenForms.OfType(Of CorrectForm)().FirstOrDefault()
                 Dim wrongForm As WrongForm = Application.OpenForms.OfType(Of WrongForm)().FirstOrDefault()
 
@@ -168,49 +168,48 @@ Public Class MCQuestion9
                     correctForm.Close()
                 End If
 
-                ' Open QuestionScores form (retain panel color)
                 Dim questionScores As QuestionScores = Application.OpenForms.OfType(Of QuestionScores)().FirstOrDefault()
                 If questionScores Is Nothing Then
                     questionScores = New QuestionScores()
-                    questionScores.Q9Panel.FillColor = Color.Gray ' Reflect the correct color
+                    questionScores.Q9Panel.FillColor = Color.Gray
                     questionScores.Show()
                 Else
-                    questionScores.Q9Panel.FillColor = Color.Gray ' Update the color for an existing form
+                    questionScores.Q9Panel.FillColor = Color.Gray 
                 End If
 
-                ' Move to the next phase
+                
                 currentPhase = 2
 
             Case 2
-                ' Close QuestionScores and transition to the next question
+                
                 Dim questionScores As QuestionScores = Application.OpenForms.OfType(Of QuestionScores)().FirstOrDefault()
                 If questionScores IsNot Nothing Then
                     questionScores.Close()
                 End If
 
-                ' Show MCQuestion10 in Guna2Panel1
+                
                 Dim mcInterface As MCInterface = Application.OpenForms.OfType(Of MCInterface)().FirstOrDefault()
                 If mcInterface IsNot Nothing Then
                     mcInterface.Guna2Panel1.Controls.Clear()
-                    Dim mcQuestion10 As New MCQuestion10() ' Replace with the actual next question form
+                    Dim mcQuestion10 As New MCQuestion10() 
                     mcQuestion10.TopLevel = False
                     mcInterface.Guna2Panel1.Controls.Add(mcQuestion10)
                     mcQuestion10.Show()
                 End If
 
-                ' Re-enable buttons
+                
                 buttonsClickable = True
 
-                ' Stop transition timer
+              
                 transitionTimer.Stop()
         End Select
     End Sub
 
-    ' Initialize timers in the constructor or Form_Load event
+   
     Private Sub MCQuestion9_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         AddHandler flickerTimer.Tick, AddressOf FlickerEffect
         AddHandler transitionTimer.Tick, AddressOf TransitionSequence
-        MCInterface.QuestionLbl.Text = "9" ' Update question label to show it's Question 9
+        MCInterface.QuestionLbl.Text = "9" 
     End Sub
 
 End Class
